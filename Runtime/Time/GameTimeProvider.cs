4a2b8b9669257889cd3efa11906c7e11fa7c1af5
Ivan Belyj <k4fd4@yandex.ru>
2025-12-08 12:59:31 +0300

    Log error if GameTimeProvider instance is null

--- Runtime/Time/GameTimeProvider.cs
+++ Runtime/Time/GameTimeProvider.cs
@@ -6,10 +6,29 @@ public class GameTimeProvider : MonoBehaviour, IGameTimeProvider
 {
     private static GameTimeProvider instance;
 
-    public static GameTimeProvider Instance => instance;
+    public static GameTimeProvider Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Debug.LogError(
+                    $"{nameof(GameTimeProvider)} is required in the scene to be used via singleton property. " +
+                    "Please, ensure it is created on the scene.");
+                return null;
+            }
+            else
+            {
+                return instance;
+            }   
+        }
+    }
 
     public static double GameTime => Instance.GetGameTime();
 
+    /// <summary>
+    /// The time in seconds since the unified game timeline started.
+    /// </summary>
     public double GetGameTime()
     {
         return NetworkTime.time;
